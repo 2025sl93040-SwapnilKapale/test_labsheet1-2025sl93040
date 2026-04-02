748ea17c7d05524e79183159d8452c8f8b82d524
Swapnil Kapale <swapnilkapaledev@gmail.com>
2026-04-03 00:02:56 +0530

    mistake

--- calculator.py
+++ calculator.py
@@ -7,7 +7,5 @@ def multiply(a, b):
 def subtract(a, b):
     return a - b
 
-def divide(a, b):
-    if b == 0:
-        return None
-    return a / b
+def add(a, b):
+    return a - b   # WRONG intentionally
